1fb20dc328c2cf4a60a7a86453acc273e68b64bf
vans <spentmny@nate.com>
2021-12-11 18:18:02 +0900

    Update found first day step

--- WeekPrint/Main.java
+++ WeekPrint/Main.java
@@ -9,6 +9,17 @@ public class Main {
         int month = sc.nextInt();
         int week = sc.nextInt();
 
+        //윤년, 평년 판단하기
+        int leapYear = 0;
+        if ((year % 4 == 0) && (year % 100 != 0)) { //윤년일때
+            leapYear = 1;
+        } else if (year % 400 == 0) { //윤년일때
+            leapYear = 1;
+        } else { //평년일때
+            leapYear = 0;
+        }
+
+        //둠스데이의 요일 구하기
         int yearTen = year % 100;
         int ankerday = 0;
         if(yearTen % 4 ==0){
@@ -21,26 +32,29 @@ public class Main {
             ankerday = 3;
         }
         int doomsDays = ((yearTen % 12) + (yearTen / 12) + ((yearTen / 12) / 4) + ankerday) % 7;
+        //0,1,2,3,4,5,6 = 일,월,화,수,목,금,토
 
+        //1월일때 1일 시작요일 구하기(둠스데이는 1월3일 윤년일땐 1월4일
+        int[][] calender = {
+                {0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0}
+        };
 
-
-
-
-        int[][] calender = new int[5][7];
-        int leapYear = 0;
-        int monthDays = 0;
-
-        int []nomalMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
-        int []leapMonth = {31,29,31,30,31,30,31,31,30,31,30,31};
-
-
-        if ((year % 4 == 0) && (year % 100 != 0)) { //윤년일때
-            leapYear = 1;
-        } else if (year % 400 == 0) { //윤년일때
-            leapYear = 1;
-        } else { //평년일때
-            leapYear = 0;
+        int []monthDaysArr = {31,0,31,30,31,30,31,31,30,31,30,31};
+        if(leapYear == 0){ //1월 3일
+            monthDaysArr[1] = 28;
+            doomsDays -= 2;
+        }else{ //1월 4일
+            monthDaysArr[1] = 29;
+            doomsDays -= 3;
         }
+
+        int monthDays = monthDaysArr[month];
+
+
 
 
 
