b94f542f0e126e96b05990bc292003db4fa95841
vans <spentmny@nate.com>
2021-12-13 17:39:41 +0900

    Refactor print step

--- WeekPrint/Save.java
+++ WeekPrint/Save.java
@@ -121,37 +121,37 @@ public class Save {
 
         switch (week) {
             case 1:
-                System.out.println(week + "주차는" + " " + calender[0][doomsDays] + "일부터" + calender[1][0] + "일까지 입니다.");
+                System.out.println(week + "주차는" + " " + calender[0][doomsDays] + "일부터 " + calender[1][0] + "일까지 입니다.");
                 break;
             case 2:
-                System.out.println(week + "주차는" + " " + calender[1][1] + "일부터" + calender[2][0] + "일까지 입니다.");
+                System.out.println(week + "주차는" + " " + calender[1][1] + "일부터 " + calender[2][0] + "일까지 입니다.");
                 break;
             case 3:
-                System.out.println(week + "주차는" + " " + calender[2][1] + "일부터" + calender[3][0] + "일까지 입니다.");
+                System.out.println(week + "주차는" + " " + calender[2][1] + "일부터 " + calender[3][0] + "일까지 입니다.");
                 break;
             case 4:
                 if (calender[4][0] == 0) {
-                    System.out.println(week + "주차는" + " " + calender[3][1] + "일부터" + calender[3][6] + "일까지 입니다.");
+                    System.out.println(week + "주차는" + " " + calender[3][1] + "일부터 " + calender[3][6] + "일까지 입니다.");
                 } else {
-                    System.out.println(week + "주차는" + " " + calender[3][1] + "일부터" + calender[4][0] + "일까지 입니다.");
+                    System.out.println(week + "주차는" + " " + calender[3][1] + "일부터 " + calender[4][0] + "일까지 입니다.");
                 }
                 break;
             case 5:
                 if (calender[4][1] == 0) {
                     System.out.println(week + "주차는" + " " + "해당 월에 존재하지 않습니다.");
                 } else if (calender[5][0] == 0) {
-                    System.out.println(week + "주차는" + " " + calender[4][1] + "일부터" + calender[lastDayX][lastDayY] + "일까지 입니다.");
+                    System.out.println(week + "주차는" + " " + calender[4][1] + "일부터 " + calender[lastDayX][lastDayY] + "일까지 입니다.");
                 } else if (calender[5][1] != 0) {
-                    System.out.println(week + "주차는" + " " + calender[4][1] + "일부터" + calender[5][0] + "일까지 입니다.");
+                    System.out.println(week + "주차는" + " " + calender[4][1] + "일부터 " + calender[5][0] + "일까지 입니다.");
                 } else {
-                    System.out.println(week + "주차는" + " " + calender[4][1] + "일부터" + calender[5][0] + "일까지 입니다.");
+                    System.out.println(week + "주차는" + " " + calender[4][1] + "일부터 " + calender[5][0] + "일까지 입니다.");
                 }
                 break;
             case 6:
                 if ((calender[5][0] == 0) || (calender[5][1] == 0)) {
                     System.out.println(week + "주차는" + " " + "해당 월에 존재하지 않습니다.");
                 } else {
-                    System.out.println(week + "주차는" + " " + calender[5][1] + "일부터" + calender[lastDayX][lastDayY] + "일까지 입니다.");
+                    System.out.println(week + "주차는" + " " + calender[5][1] + "일부터 " + calender[lastDayX][lastDayY] + "일까지 입니다.");
                 }
                 break;
         }
